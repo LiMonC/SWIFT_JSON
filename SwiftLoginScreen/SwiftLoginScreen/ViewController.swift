38a0b2b20fbe65b3e7db77ea8ce351328bfbcdaf
Ligia Montejo <ligia.montejoc@gmail.com>
2015-06-26 18:08:34 -0500

    Proyectos

--- SwiftLoginScreen/SwiftLoginScreen/ViewController.swift
+++ SwiftLoginScreen/SwiftLoginScreen/ViewController.swift
@@ -14,12 +14,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var username: UILabel!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        //self.performSegueWithIdentifier("goto_login", sender: self)
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.username.text = prefs.valueForKey("USERNAME") as! NSString as String
+        }
+    }
 
+    @IBAction func logoutTapped(sender: UIButton) {
+         //self.performSegueWithIdentifier("goto_login", sender: self)
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
 
 }
 
--- Tutorial-JSON-Alamofire/Tutorial-JSON-Alamofire/ViewController.swift
+++ Tutorial-JSON-Alamofire/Tutorial-JSON-Alamofire/ViewController.swift
@@ -7,17 +7,53 @@
 //
 
 import UIKit
+//import Alamofire
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fecha: UILabel!
+    
+    @IBOutlet weak var ipAddress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getFecha()
+        getIP()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getFecha()
+    {
+        Alamofire.request(.GET, "http://date.jsontest.com").responseJSON() {
+            (_, _, JSON, error) in
+            if error == nil {
+                let info =  JSON as! NSDictionary
+                self.fecha.text = info["date"] as? String
+                println(JSON)
+            } else {
+                println(error)
+            }
+        }
+    }
+    
+    func getIP(){
+        Alamofire.request(.GET, "http://ip.jsontest.com").responseJSON() {
+            (_, _, JSON, error) in
+            if error == nil {
+                let info =  JSON as! NSDictionary
+                self.ipAddress.text = info["ip"] as? String
+                println(JSON)
+            } else {
+                println(error)
+            }
+        }
+
     }
 
 
